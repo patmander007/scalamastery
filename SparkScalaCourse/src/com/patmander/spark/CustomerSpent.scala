8505a6e08334464b7334e992dde64168c836f262
RCGEnableODCDeveloper <rcgenableodcdeveloper@rcggs.com>
2017-12-22 08:15:47 +0800

    customerSpent

--- SparkScalaCourse/src/com/patmander/spark/CustomerSpent.scala
+++ SparkScalaCourse/src/com/patmander/spark/CustomerSpent.scala
@@ -15,7 +15,16 @@ object CustomerSpent {
 
     val customerExpenses = lines.map(line => line.split(",")).map(csvLine => (csvLine(0).toInt, csvLine(2).toFloat)).reduceByKey((val1, val2) => val1 + val2).sortByKey()
     
-    customerExpenses.collect().foreach(x=> println(f"customerId: ${x._1}, total: ${x._2}%.2f"))
+    
+    println("Printing values sorted by customer ID") 
+    
+//    customerExpenses.collect().foreach(x=> println(f"customerId: ${x._1}, total: ${x._2}%.2f"))
+    
+    val customerExpensesSortedByAmount = customerExpenses.map(x=>(x._2, x._1)).sortByKey().map(x=>(x._2, x._1))
+    
+    println("Printing values sorted by amount desc") 
+    
+    customerExpensesSortedByAmount.collect().foreach(x=> println(f"customerId: ${x._1}, total: ${x._2}%.2f"))
 
   }
 
